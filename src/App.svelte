bc11bc6f395efe606170413815ba3eb00782bc7f
William Perron <thewilliamperron@gmail.com>
2020-01-31 21:01:57 -0500

    Finished first blog post
    finished the Reading List blog post and added a simple
    markdown to json converter, also used in github actions.
    
    Also added routing components to svelte app
    
    Note: still missing breadcrumbs, navigation to homepage and
    a way to fix the refresh problem (redirect to App.svelte for every
    route)

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
+	import { Router, Link, Route } from "svelte-routing"
 	import Title from './components/Title.svelte'
 	import Social from './components/Social.svelte'
-	import ContentList from './components/ContentList.svelte'
+	import Post from './Post.svelte'
 
 	let links = [
 		{
@@ -15,30 +16,69 @@
 	]
 
 	let promise = fetchPosts()
+	let posts = null
 
 	async function fetchPosts() {
-		return await fetch('/posts.json')
+		let res = await fetch('/posts.json')
 			.then(res => res.json())
+		posts = res
+		return res
+	}
+
+	function getPostBySlug(slug) {
+		const match = posts.filter(post => post.attributes.slug === slug)
+		if (match.length > 1) {
+			throw new Error('Found more than one slug, expected only one.')
+		}
+		return match[0]
 	}
 </script>
 
 <main>
 	<Title title='wperron' src='profile-small.jpg' desc="that's me!" />
 	<hr />
-	<Social links={links} />
-	<hr />
-	{#await promise}
-		<p>waiting...</p>
-	{:then posts}
-		<ContentList posts={posts} />
-	{:catch err}
-		<p>Oops, something went wrong... 😬</p>
-	{/await}
+	<Router>
+		<Route path="/">
+			<Social links={links} />
+			<hr />
+			{#await promise}
+				<p>waiting...</p>
+			{:then posts}
+				{#each posts as post}
+					<div class='content-item'>
+						<Link to={`/blog/${post.attributes.slug}`}>
+							<span class='content-title'>{post.attributes.title}</span>
+						</Link>
+						<p class='content-text'>{post.attributes.date}</p>
+						<p class='content-text'>{post.attributes.description}</p>
+					</div>
+				{/each}
+			{:catch err}
+				<p>Oops, something went wrong... 😬</p>
+			{/await}
+		</Route>
+		<Route path="blog/:slug" let:params>
+			<Post post={getPostBySlug(params.slug)} />
+		</Route>
+	</Router>
 </main>
 
 <style>
 	main {
 		max-width: 650px;
 		margin: 0 auto;
+	}
+
+	.content-item {
+		cursor: pointer;
+	}
+
+	.content-title {
+		margin: 10;
+		font-size: 1.5rem;
+	}
+
+	.content-text {
+		margin: 0;
 	}
 </style>
---
+++ src/Post.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let post
+</script>
+
+<div class='blog-post'>
+	<h1>{post.attributes.title}</h1>
+	<i>{post.attributes.date}</i>
+	<p>{post.attributes.description}</p>
+	<hr />
+	<div class='blog-post-body'>
+		{@html post.body}
+	</div>
+</div>
