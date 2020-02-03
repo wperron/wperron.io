5aafe529866bf3c732acf3343f9f7a65a6a5df03
William Perron <wperron@mediagrif.com>
2020-02-03 15:47:22 -0500

    updated display to flex on title component
    * updated the display property on title container
      to 'display: flex;'
    * added a media query to avoid resizing the profile
      image on smaller viewports and overall provide
      a more enjoyable mobile experience

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -8,24 +8,46 @@
 	<img src={src} alt={desc} width="100px" />
 	<div>
 		<h1>{title}</h1>
-		<p>Software Developer, DevOps Advocate and Cloud Computing Enthusiast.</p>
+		<p>Software Developer, DevOps Advocate, Git ninja and Cloud Computing Enthusiast.</p>
 	</div>
 </section>
 
 <style>
 	.title {
-		display: block;
+		display: flex;
 		margin: 30px 0;
 	}
 
 	.title img {
-		float: left;
 		margin-right: 20px;
+		display: block;
+		max-width:230px;
+		max-height:95px;
+		min-width: 100px;
+		width: auto;
+		height: auto;
 	}
 
 	.title h1 {
 		font-size: 3em;
 		font-weight: 600;
 		margin: 0;
+	}
+
+	@media only screen and (max-width: 400px) {
+		.title {
+			display: flex;
+			flex-direction: column;
+			margin: 30px 0;
+		}
+
+		.title img {
+			margin: auto;
+			display: block;
+			max-width:230px;
+			max-height:95px;
+			width: auto;
+			height: auto;
+		}
 	}
 </style>
