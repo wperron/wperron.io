037a3d534b2bdf4a9e5f7d4ca75d9d1dc40f5d1c
William Perron <thewilliamperron@gmail.com>
2020-02-05 19:41:23 -0500

    refactored posts to use svelte readable store
    Refactored the posts fetch operation to a readable store
    and removed the {#await} block since it was no longer used.
    
    Also refactored the links to the same store file for cleanliness.
    
    Closes #2

--- src/Post.svelte
+++ src/Post.svelte
@@ -1,7 +1,17 @@
 <script>
+	import { posts } from './stores.js'
 	import Date from './components/Date.svelte'
 
-	export let post
+	export let slug
+
+	let post = null
+	const unsubscribe = posts.subscribe(all => {
+		const match = all.filter(x => x.attributes.slug === slug)
+		if (match.length > 1) {
+			throw new Error('Found more than one lsug, expected only one.')
+		}
+		post = match[0]
+	})
 </script>
 
 <div class='blog-post'>
---
+++ src/stores.js
@@ -0,0 +1,22 @@
+import { readable } from 'svelte/store';
+
+export const posts = readable([], async function start(set) {
+	const res = await fetch('/posts.json')
+		.then(res => res.json())
+	set(res)
+
+	return function stop() {
+		return
+	}
+})
+
+export const links = [
+	{
+		name: "GitHub",
+		href: "https://github.com/wperron",
+	},
+	{
+		name: "Twitter",
+		href: "https://twitter.com/_wperron",
+	},
+]
